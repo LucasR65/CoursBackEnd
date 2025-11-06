6fbbdea224ba8b768060f31261c204d5814290d5
LucasR65 <158263593+LucasR65@users.noreply.github.com>
2025-11-06 17:05:45 +0100

    modif swagger mongo

--- Exo2_Rosier_Lucas/server.mongo.js
+++ Exo2_Rosier_Lucas/server.mongo.js
@@ -1,8 +1,11 @@
+// server.mongo.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import swaggerUi from "swagger-ui-express";
+import { createRequire } from "module";
 
 dotenv.config();
 connectDB();
@@ -10,8 +13,15 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Charger swagger.json depuis ESM avec require
+const require = createRequire(import.meta.url);
+const swaggerDocument = require("./swagger.json");
+
 app.use(cors());
 app.use(express.json());
+
+// Swagger à la racine
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("API ToDoList avec MongoDB");
@@ -20,5 +30,6 @@ app.get("/", (req, res) => {
 app.use("/tasks", taskRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Serveur Mongo sur http://localhost:${PORT}`);
+  console.log(`Serveur sur http://localhost:${PORT}`);
+  console.log(`Docs Swagger sur http://localhost:${PORT}/api-docs`);
 });
