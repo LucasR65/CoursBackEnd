e77619851bfdb9354d26118b32d619a2ec9280f2
LucasR65 <158263593+LucasR65@users.noreply.github.com>
2025-11-10 11:31:27 +0100

    modif pg server file for swagger

--- Exo2_Rosier_Lucas/server.pg.js
+++ Exo2_Rosier_Lucas/server.pg.js
@@ -1,24 +1,39 @@
+// server.pg.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectPG } from "./config/db.pg.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import swaggerUi from "swagger-ui-express";
+import { createRequire } from "module";
 
 dotenv.config();
-connectPG(); // Connexion PostgreSQL
+connectPG(); 
 
 const app = express();
 const PORT = process.env.PORT_PG || 5001;
 
+const require = createRequire(import.meta.url);
+const swaggerDocument = require("./swagger.json");
+
 app.use(cors());
 app.use(express.json());
+
+// Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Bienvenue sur l'API ToDoList Express avec PostgreSQL");
 });
 
+// Routes utilisateurs
+app.use("/api/users", userRoutes);
+
+// Routes tasks
 app.use("/tasks", taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`Serveur PostgreSQL sur http://localhost:${PORT}`);
+  console.log(`Docs Swagger sur http://localhost:${PORT}/api-docs`);
 });
